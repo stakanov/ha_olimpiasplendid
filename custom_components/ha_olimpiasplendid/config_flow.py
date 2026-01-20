43739b1420fd29e7762005d8c42c1d7d45104f4f
stakanov <senatoredaniele@gmail.com>
2026-01-20 15:02:50 +0100

    Add config flow for HA OlimpiaSplendid
    Implement config flow for HA OlimpiaSplendid integration.

--- custom_components/ha_olimpiasplendid/config_flow.py
+++ custom_components/ha_olimpiasplendid/config_flow.py
@@ -1,1 +1,11 @@
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class HAOlimpiaSplendidConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for My Integration."""
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="HA OlimpiaSplendid", data=user_input)
+        return self.async_show_form(step_id="user", data_schema={})
 
