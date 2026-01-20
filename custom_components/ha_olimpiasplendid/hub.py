c86d76a955cf796eb25a4b68717341abc9a0dbf3
stakanov <senatoredaniele@gmail.com>
2026-01-20 15:13:02 +0100

    Add OlimpiaSplendidHub class with register methods

--- custom_components/ha_olimpiasplendid/hub.py
+++ custom_components/ha_olimpiasplendid/hub.py
@@ -1,1 +1,10 @@
+class OlimpiaSplendidHub:
+    def __init__(self, client, slave):
+        self.client = client
+        self.slave = slave
 
+    async def read_register(self, address):
+        return await self.client.read_holding_registers(address, 1, unit=self.slave)
+
+    async def write_register(self, address, value):
+        await self.client.write_register(address, value, unit=self.slave)
