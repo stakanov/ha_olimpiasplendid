b7e3622a453b3751d700d9e199b46d876654d986
stakanov <senatoredaniele@gmail.com>
2026-01-20 15:01:37 +0100

    Update __init__.py

--- custom_components/ha_olimpiasplendid/__init__.py
+++ custom_components/ha_olimpiasplendid/__init__.py
@@ -1,1 +1,15 @@
+"""Olimpia Splendid modbus integration for Home Assistant."""
 
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "ha_olimpiasplendid"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration via configuration.yaml (optional)."""
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry):
+    """Set up the integration via UI."""
+    hass.data[DOMAIN] = {}
+    return True
